1f9d7520f3f1906830174af9908b7384ed9c7853
Ethan Black <ethanrossblack@gmail.com>
2023-12-25 17:06:02 -0700

    feat: add enum to vote column in LegislatorVote

--- app/models/legislator_vote.rb
+++ app/models/legislator_vote.rb
@@ -1,4 +1,11 @@
 class LegislatorVote < ApplicationRecord
   belongs_to :legislator
   belongs_to :roll_call
+
+  enum vote: {
+    "Aye": 1,
+    "Nay": 2,
+    "Excused": 3,
+    "Absent": 4
+  }
 end
