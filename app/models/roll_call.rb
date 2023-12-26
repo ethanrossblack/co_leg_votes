1d099f1404280442b798a57cfcb609c91aad21f9
Ethan Black <ethanrossblack@gmail.com>
2023-12-25 17:14:42 -0700

    feat: aye/nay/excused/absent_voters instance methods for roll_calls plus placeholder tests

--- app/models/roll_call.rb
+++ app/models/roll_call.rb
@@ -2,4 +2,29 @@ class RollCall < ApplicationRecord
   belongs_to :bill
   has_many :legislator_votes
   has_many :legislators, through: :legislator_votes
+
+  enum chamber: {
+    "House": 21,
+    "Senate": 22
+  }
+
+  def aye_voters
+    # 1 is the legislator_vote enum value for "Aye"
+    legislators.where(legislator_votes: { vote: 1 }).order(:party)
+  end
+  
+  def nay_voters
+    # 2 is the legislator_vote enum value for "Nay"
+    legislators.where(legislator_votes: { vote: 2 })
+  end
+  
+  def excused_voters
+    # 3 is the legislator_vote enum value for "Excused"
+    legislators.where(legislator_votes: { vote: 3 })
+  end
+  
+  def absent_voters
+    # 4 is the legislator_vote enum value for "Absent"
+    legislators.where(legislator_votes: { vote: 4 })
+  end
 end
--- spec/models/roll_call_spec.rb
+++ spec/models/roll_call_spec.rb
@@ -6,4 +6,30 @@ RSpec.describe RollCall, type: :model do
     it { should have_many :legislator_votes }
     it { should have_many(:legislators).through(:legislator_votes) }
   end
+
+  describe 'instance methods' do
+    describe '#aye_voters' do
+      xit 'returns an array of Legislators who voted "Aye" on the roll_call' do
+
+      end
+    end
+
+    describe '#nay_voters' do
+      xit 'returns an array of Legislators who voted "Nay" on the roll_call' do
+
+      end
+    end
+
+    describe '#excused_voters' do
+      xit 'returns an array of Legislators marked as "Excused" on the roll_call' do
+
+      end
+    end
+
+    describe '#absent_voters' do
+      xit 'returns an array of Legislators marked as "Absent" on the roll_call' do
+
+      end
+    end
+  end
 end
